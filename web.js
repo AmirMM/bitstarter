ecf0cf5dc83947dd0d8b9fc7fdb153a3971402e4
Amir M <amir.m@aminor-tech.com>
2013-07-09 19:16:15 +0000

    add require buffer

--- web.js
+++ web.js
@@ -1,6 +1,5 @@
 
 var fs = require('fs');
-//var buff = require('buffer');
 
 var inputFile = 'index.html';
 
@@ -11,8 +10,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-response.send(inputBuff.toString());
+    response.send(inputBuff.toString());
 });
 
 var port = process.env.PORT || 5000;
