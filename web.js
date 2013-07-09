163fba965942a2df8afcb208cf6a87c04d0c78fa
Amir M <amir.m@aminor-tech.com>
2013-07-09 19:03:01 +0000

    Reading index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+
+var fs = require('fs');
+//var buff = require('buffer');
+
+var inputFile = 'index.html';
+
+var inputBuff = fs.readFileSync(inputFile);
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+response.send(inputBuff.toString);
 });
 
 var port = process.env.PORT || 5000;
